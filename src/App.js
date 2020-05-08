5cae9c0df7d3b3387fd6aeab1e61f19958c582bb
khaldmansour <khaldmansour93@gmail.com>
2020-05-08 14:57:15 +0200

    Todos

--- src/App.js
+++ src/App.js
@@ -1,26 +1,88 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { Component } from 'react';
+import Todos from './components/Todos';
 import './App.css';
+import AddItem from './components/addItem';
+// import uuid from 'uuid/v1'
+import uuid from 'react-uuid';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+
+
+class App extends Component
+{
+  state = {
+    todos:[
+      {
+        id: 1,
+        title: 'cleaning',
+        completed: false,
+      },
+      {
+        id: 2,
+        title: 'washing',
+        completed: true,
+      },
+      {
+        id: 3,
+        title: 'playing',
+        completed: false,
+      },
+    ]
+  }
+
+  delItem = (id) => {
+    this.setState({ todos:[...this.state.todos.filter(todo => todo.id !== id)]});
+
+  }
+
+  //Toggle Done
+
+  markDone = (id) =>
+   {
+     console.log(id);
+     
+     this.setState({ todos: this.state.todos.map(todo =>
+      {
+           if(todo.id === id)
+           {
+             todo.completed = !todo.completed
+           }
+           return todo;
+         })
+       }
+     )
+       
+   }
+
+   addTodo = (title) => {
+    const newTodo = {
+      id: uuid(),
+      title: title,
+      completed:false
+    }
+    this.setState({ todos: [...this.state.todos , newTodo]})
+    console.log(title)
+  }
+
+   
+
+  render()
+  {
+
+    return (
+      <div className="App">
+          
+         <Todos todos={this.state.todos} markDone={this.markDone} delItem={this.delItem}/>
+         <AddItem  addTodo={this.addTodo}/>
+
+      </div>
+    );
+
+  }
+  
 }
+
+
+
 
 export default App;
